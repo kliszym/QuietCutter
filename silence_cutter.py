e488ed980ec0e287680253edcfa11419606b37ff
Szymon Klimek <kliszym@gmail.com>
2019-12-11 00:50:19 +0100

    added example

--- silence_cutter.py
+++ silence_cutter.py
@@ -15,7 +15,38 @@ def categorize(out_array, in_value):
         out_array["4"] += 1
 
 
-samples_rate, samples = wav.read(filename="sample.wav", mmap=True)
+def replace_max1(out_maxim, new_max, new_index):
+    out_maxim["max1"] = new_max
+    out_maxim["index1"] = new_index
+
+
+def replace_max2(out_maxim, new_max, new_index):
+    out_maxim["max2"] = new_max
+    out_maxim["index2"] = new_index
+
+
+def find_highest_index(in_array):
+    maxim = {
+        "max1": 0,
+        "index1": 0,
+        "max2": 0,
+        "index2": 0
+    }
+
+    for max_iterator in range(5):
+        if in_array[str(max_iterator)] > maxim["max1"]:
+            replace_max2(maxim, maxim["max1"], maxim["index1"])
+            replace_max1(maxim, in_array[str(max_iterator)], max_iterator)
+        elif in_array[str(max_iterator)] > maxim["max2"]:
+            replace_max2(maxim, in_array[str(max_iterator)], max_iterator)
+
+    if maxim["index2"] > 1:
+        if maxim["index1"] < maxim["index2"]:
+            return maxim["index1"]
+    return maxim["index2"]
+
+
+samples_rate, samples = wav.read(filename="sample_small.wav", mmap=True)
 samples_max = np.iinfo(samples.dtype).max
 
 array = {
@@ -36,7 +67,6 @@ for sample in samples[:, 0]:
     categorize(array, value)
     i += 1
     print(str(int((i/size)*100)) + "%")
-#    print(value > 0.5)
 
 print(array)
 print(dir(samples))
